7ec2a48d19d8ac9f769cbedcb2c401ddf8199303
Adjifatou30 <adjifatoudiop@esp.sn>
2025-12-14 22:24:29 +0100

    Exercice 1 : API minimale avec EF Core InMemory et CRUD Pizza

--- Program.cs
+++ Program.cs
@@ -1,6 +1,78 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using PizzaStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// EF Core InMemory
+builder.Services.AddDbContext<PizzaDb>(
+    options => options.UseInMemoryDatabase("items")
+);
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "PizzaStore API",
+        Description = "Making the Pizzas you love",
+        Version = "v1"
+    });
+});
+builder.WebHost.UseUrls("http://localhost:5159");
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+    });
+}
+
+// GET all pizzas
+app.MapGet("/pizzas", async (PizzaDb db) =>
+    await db.Pizzas.ToListAsync()
+);
+
+// POST pizza
+app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
+
+// GET by id
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) =>
+    await db.Pizzas.FindAsync(id)
+);
+
+// PUT
+app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatepizza, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+
+    pizza.Name = updatepizza.Name;
+    pizza.Description = updatepizza.Description;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+// DELETE
+app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+
+    db.Pizzas.Remove(pizza);
+    await db.SaveChangesAsync();
+    return Results.Ok();
+});
 
 app.Run();
